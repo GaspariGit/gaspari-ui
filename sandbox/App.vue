8fa479b147209dd05cdffde7842fbdafba51844a
Enrico <enrico.paolazzi@mycity.it>
2022-11-14 11:33:28 +0100

    Initialized sandbox, fixed styles

--- sandbox/App.vue
+++ sandbox/App.vue
@@ -1,33 +1,18 @@
 <template>
-    <main>
-        <h1>Welcome to your your sandbox environment</h1>
-
-        <generic-input 
-			label="Test"
-			type="text"
-			placeholder="Inserisci"
-			v-model="value"
-		/>
-
-		{{ value }}
-        
+    <main>      
+        <HelloWorld />
     </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import GenericInput from '../src/ui/GenericInput.vue';
+import HelloWorld from './HelloWorld.vue';
 
 export default defineComponent({
   	name: 'App',
     components: {
-      GenericInput
+      HelloWorld
     },
-	data() {
-		return {
-			value: ''
-		}
-	}
 })
 </script>
 
